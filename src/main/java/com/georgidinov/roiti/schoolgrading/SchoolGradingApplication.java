ddb751603be714d4a8c5bb1e973281025a823251
Georgi Dinov <georgidinov1@abv.bg>
2021-02-09 15:32:39 +0200

    Initial Commit

---
+++ src/main/java/com/georgidinov/roiti/schoolgrading/SchoolGradingApplication.java
@@ -0,0 +1,13 @@
+package com.georgidinov.roiti.schoolgrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolGradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolGradingApplication.class, args);
+    }
+
+}
