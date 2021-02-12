fc72dc802b8bf0fabf5bfec21085cda993a5d6ac
Georgi Dinov <georgidinov1@abv.bg>
2021-02-12 17:40:12 +0200

    Added student controller and tests.

---
+++ src/main/java/com/georgidinov/roiti/schoolgrading/controller/StudentController.java
@@ -0,0 +1,44 @@
+package com.georgidinov.roiti.schoolgrading.controller;
+
+
+import com.georgidinov.roiti.schoolgrading.api.v1.model.StudentDTO;
+import com.georgidinov.roiti.schoolgrading.api.v1.model.StudentListDTO;
+import com.georgidinov.roiti.schoolgrading.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentController {
+
+    //== fields ==
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public StudentListDTO findAllStudents() {
+        log.info("StudentController::findAllStudents");
+        return this.studentService.findAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public StudentDTO findStudentById(@PathVariable String id) {
+        log.info("StudentController::findStudentById -> id passed = {}", id);
+        return this.studentService.findStudentById(Long.valueOf(id));
+    }
+
+}
---
+++ src/test/java/com/georgidinov/roiti/schoolgrading/controller/StudentControllerTest.java
@@ -0,0 +1,68 @@
+package com.georgidinov.roiti.schoolgrading.controller;
+
+import com.georgidinov.roiti.schoolgrading.api.v1.model.StudentDTO;
+import com.georgidinov.roiti.schoolgrading.api.v1.model.StudentListDTO;
+import com.georgidinov.roiti.schoolgrading.service.StudentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class StudentControllerTest {
+
+    @Mock
+    StudentService studentService;
+
+    @InjectMocks
+    StudentController studentController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(this.studentController).build();
+    }
+
+    @Test
+    void findAllStudents() throws Exception {
+        //given
+        List<StudentDTO> students = new ArrayList<>();
+        students.add(new StudentDTO("John Doe"));
+        students.add(new StudentDTO("Anna Smith"));
+        students.add(new StudentDTO("Lisa James"));
+        when(this.studentService.findAllStudents()).thenReturn(new StudentListDTO(students));
+
+        //when then
+        mockMvc.perform(get("/api/v1/student").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.students", hasSize(3)));
+    }
+
+    @Test
+    void findStudentById() throws Exception {
+        //given
+        StudentDTO studentDTO = new StudentDTO("John Doe");
+        when(this.studentService.findStudentById(anyLong())).thenReturn(studentDTO);
+
+        //when then
+        mockMvc.perform(get("/api/v1/student/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(studentDTO.getName())));
+    }
+}
